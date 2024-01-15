0feecc0e7502093627eafc5bdd8c73f89e6daa59
Matthew Lewis <mlewis@singular.co.za>
2024-01-15 16:18:05 +0200

    Temporary removal of the test project to try and fix duplicate project folders/files

--- challenges.tests/Helpers/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-// Testing framework
-global using Xunit;
-
-// Mocking library
-global using NSubstitute;
-global using NSubstitute.Extensions;
-
-// Nicer assertions
-global using FluentAssertions;
